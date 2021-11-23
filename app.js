3843d73ec3f4da56803a3822b813f8303fdeaa1c
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-11-23 15:43:50 +0600

    make it better

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+
+// const userRouter = require('./routes/userRoutes');
+// const groupRouter = require('./routes/groupRoutes');
+// const studentRouter = require('./routes/studentRoutes');
+
+const app = express();
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+
+if (process.env.NODE_ENV === 'development') {
+ app.use(morgan('dev'));
+}
+
+  app.use(express.json({ limit: '10kb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+  app.use(cookieParser());
+
+//   app.use('/users', userRouter);
+//   app.use('/groups', groupRouter);
+//   app.use('/students', studentRouter);
+  
+  
+  module.exports = app;
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+
+const port = process.env.PORT || 5000
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
